cf80650b9025d14f249d20cc87d73e166379b14b
Matteo Valenzi <dilloawallace@hotmail.com>
2025-04-28 12:58:06 +0200

    .

--- Program.cs
+++ Program.cs
@@ -1,75 +1,15 @@
-﻿using System.ComponentModel;
-
-void Saluta()
+﻿int AggiungiUno(int numero)
 {
-    Console.WriteLine($"Ciao, sono le {DateTime.Now}");
-    // Thread.Sleep(1000);
+    return numero + 1;
 }
 
-Saluta();
+int due = 2;
+int tre = AggiungiUno(due);
 
-for (int i = 0; i < 4; i++)
+Console.WriteLine($"Aggiungi 1: {tre}");
+
+int Somma(int addendo1, int addendo2)
 {
-    Saluta();
+    return addendo1 + addendo2;
 }
-
-Console.WriteLine("====================================");
-
-// valori di ritorno
-string RitornaUnSaluto()
-{
-    return $"Ciao, sono le {DateTime.Now}";
-}
-
-for (int i = 0; i < 3; i++)
-{
-    string saluto = RitornaUnSaluto();
-    Console.WriteLine(saluto);
-    // Thread.Sleep(1000);
-}
-
-int TiraUnDado()
-{
-    Random rnd = new Random();
-
-    int numero = rnd.Next(1, 7);
-
-    return numero;
-}
-
-for (int i = 0; i < 10; i++)
-{
-    Console.WriteLine($"Numero random: {TiraUnDado()}");
-}
-
-// tupla
-// la tupla fornisce una sintassi concisa per raggruppare dati
-(int, int) tupla = (3, 7);
-var tupla2 = (4, 9.5m, "ciao");
-Console.WriteLine($"Valore {tupla2.Item2} di tipo {tupla2.Item2.GetType()}");
-
-(int, int) TiraDueDadi()
-{
-    int n1 = TiraUnDado();
-    int n2 = TiraUnDado();
-
-    return (n1, n2);
-}
-
-var r = TiraDueDadi();
-Console.WriteLine($"Risultato: {r}");
-Console.WriteLine($"Numero 1: {r.Item1}");
-Console.WriteLine($"Numero 2: {r.Item2}");
-
-(int dado1, int dado2) TiraDueDadiConNome()
-{
-    int n1 = TiraUnDado();
-    int n2 = TiraUnDado();
-
-    return (dado1: n1, dado2: n2);
-}
-
-var rn = TiraDueDadiConNome();
-Console.WriteLine($"Risultato: {rn}");
-Console.WriteLine($"Numero 1: {rn.dado1}");
-Console.WriteLine($"Numero 2: {rn.dado2}");
+Console.WriteLine($"Somma: {Somma(7, 9)}");
