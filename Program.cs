3a3c963aaa21419993e56781b4ce8aec8ef46ce7
Matteo Valenzi <dilloawallace@hotmail.com>
2025-05-05 12:11:40 +0200

    .

--- Program.cs
+++ Program.cs
@@ -1,95 +1,30 @@
-﻿using System.Net.WebSockets;
+﻿// lambda
+// serve per creare funzioni anonime (usa e getta)
+// action (non ritorna un valore)
+// func (ritorna un valore)
 
-int AggiungiUno(int numero)
+List<int> numeri = [1, 2, 3, 4];
+
+numeri.ForEach(x =>
 {
-    return numero + 1;
+    Console.WriteLine(x * x);
+});
+
+// 
+var quadrati = numeri.Select(x => x * x); // [1,4,9,16]
+Console.WriteLine(string.Join(",", quadrati));
+
+// funzioni ricorsive
+// funzione che richiama se stessa
+
+// primi 5 numeri => 5+4+3+2+1 = 15
+int SommaPrimiNNumeri(int n)
+{
+    if (n == 0)
+        return 0;
+    else
+        return n + SommaPrimiNNumeri(n - 1);
 }
 
-int due = 2;
-int tre = AggiungiUno(due);
-
-Console.WriteLine($"Aggiungi 1: {tre}");
-
-int Somma(int addendo1, int addendo2)
-{
-    return addendo1 + addendo2;
-}
-
-Console.WriteLine($"Somma: {Somma(7, 9)}");
-
-int r = Somma(addendo2: 4, addendo1: 7);
-
-// C# passa i parametri come valore di default
-// cioè di default, passa una copia della variabile
-
-void quadratoByVal(int valParameter)
-{
-    valParameter *= valParameter;
-}
-
-int numero = 4;
-quadratoByVal(numero);
-Console.WriteLine(numero);
-
-// ref passa un riferimento ad una variabile
-void quadratoByRef(ref int refParameter)
-{
-    refParameter *= refParameter;
-}
-quadratoByRef(ref numero);
-Console.WriteLine(numero);
-
-// out serve da segnaposto per un valore ritornato dalla funzione
-string dieci = "dieci";
-int risultato;
-bool ok = int.TryParse(dieci, out risultato);
-
-if (ok)
-{
-    Console.WriteLine($"{dieci} convertito con successo in {risultato}");
-}
-else
-{
-    Console.WriteLine($"Non sono riuscito a convertire {dieci} in numero");
-}
-
-// numero parametri variabile
-int SommaTutto(params int[] numeri)
-{
-    int r = 0;
-    foreach (var n in numeri)
-    {
-        r += n;
-    }
-
-    return r;
-}
-
-Console.WriteLine(SommaTutto(3, 56, 78, 34, 56, 7, 7));
-
-int[] prezzi = [4, 67, 34, 56, 1];
-Console.WriteLine(SommaTutto(prezzi));
-
-// parametri di default (facoltativi)
-void Saluta(string nome = "a tutti")
-{
-    Console.WriteLine($"Ciao {nome}");
-}
-Saluta("Gigi");
-Saluta();
-
-// i valori di default, ma anche params, vanno dichiarati come ultimi (posizione) parametri della funzione
-void SalutaNVolte(string nome, string saluto = "Ciao", int volte = 3)
-{
-    for (int i = 0; i < volte; i++)
-    {
-        Console.WriteLine($"{saluto} {nome}");
-    }
-}
-
-SalutaNVolte("Bruno");
-SalutaNVolte("Mario", "Salve");
-SalutaNVolte("Anna", "Salve", 8);
-
-// Salutare 5 volte "Luisa" (usando Ciao come valore di default)
-SalutaNVolte("Luisa", volte: 5);
+int r = SommaPrimiNNumeri(3);
+Console.WriteLine(r);
