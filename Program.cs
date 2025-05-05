d24cee16f7ad8a150238d2ec53e56c64c5f189cd
Matteo Valenzi <dilloawallace@hotmail.com>
2025-05-05 12:57:48 +0200

    .

--- Program.cs
+++ Program.cs
@@ -1,30 +1,43 @@
-﻿// lambda
-// serve per creare funzioni anonime (usa e getta)
-// action (non ritorna un valore)
-// func (ritorna un valore)
-
-List<int> numeri = [1, 2, 3, 4];
-
-numeri.ForEach(x =>
+﻿// filesystem => files e cartelle del computer
+void LogTitolo(string titolo)
 {
-    Console.WriteLine(x * x);
-});
-
-// 
-var quadrati = numeri.Select(x => x * x); // [1,4,9,16]
-Console.WriteLine(string.Join(",", quadrati));
-
-// funzioni ricorsive
-// funzione che richiama se stessa
-
-// primi 5 numeri => 5+4+3+2+1 = 15
-int SommaPrimiNNumeri(int n)
-{
-    if (n == 0)
-        return 0;
-    else
-        return n + SommaPrimiNNumeri(n - 1);
+    Console.WriteLine("");
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine(titolo + ":");
+    Console.ResetColor();
 }
 
-int r = SommaPrimiNNumeri(3);
-Console.WriteLine(r);
+// leggere contenuto di una cartella
+
+// cartella corrente
+LogTitolo("root");
+var root = Directory.GetCurrentDirectory();
+Console.WriteLine(root);
+
+// informazioni sulla directory
+var rootInfo = new DirectoryInfo(root);
+LogTitolo("rootInfo.Name");
+Console.WriteLine(rootInfo.Name);
+LogTitolo("rootInfo.Parent");
+Console.WriteLine(rootInfo.Parent);
+
+// trovare le sottodirectory
+// var dirs = Directory.GetDirectories(root); // percorso di directory assoluto
+// var dirs = Directory.GetDirectories("documenti"); // percorso di directory relativo
+var dirs = Directory.GetDirectories("documenti", "prev*", SearchOption.AllDirectories); // percorso di directory relativo
+
+// overloads
+// modi diversi (elenco di parametri) di chiamare la setssa funzione
+
+LogTitolo("Lista directories");
+foreach (var dir in dirs)
+{
+    Console.WriteLine(dir);
+}
+
+LogTitolo("Lista directories info");
+var dirsInfo = rootInfo.GetDirectories();
+foreach (var di in dirsInfo)
+{
+    Console.WriteLine(di.Name);
+}
